b15f2becbf20beaf73c13ec980efab28f7c1d21b
Olli <oltsuwh@gmail.com>
2018-07-17 09:23:48 +0300

    Initial commit, added Procfile

---
+++ index.js
@@ -0,0 +1,56 @@
+// Imports
+const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const captionService = require('./services/captionService')
+const moment = require('moment')
+
+// Create app
+const app = express()
+
+// Middleware
+app.use(cors())
+
+// Local configuration
+require('dotenv').config()
+
+// Get youtube captions via video id
+app.get('/captions', async (req, res) => {
+  const { id, target } = req.query
+
+  if (!id) {
+    return res.status(422).send('No video id was provided.')
+  }
+  // TEST ID: iigDXgdKkuI
+  try {
+    const unfilteredCaptions = await captionService.getCaptions(id)
+
+    // Filter captions and sort them by timestamp
+    const matchingCaptions = unfilteredCaptions.filter(({ captions }) => {
+      for (let i = 0; i < captions.length; i++) {
+        if (captions[i].trim().length === 0) {
+          return false
+        }
+        if (captions[i].trim().toLowerCase().indexOf(target.trim().toLowerCase()) > -1) {
+          return true
+        }
+      }
+      
+      return false
+    }).sort((a, b) => {
+      return moment(a.timestamp, 'hh:mm:ss.SSZ').valueOf() - moment(b.timestamp, 'hh:mm:ss.SSZ').valueOf()
+    })
+
+    return res.json(matchingCaptions)
+  } catch (e) {
+    console.error('error', e)
+    return res.status(422).send('Error fetching captions.')
+  }
+})
+
+// Initialize server
+const PORT = process.env.PORT || 8080
+const server = http.createServer(app)
+server.listen(PORT, () => {
+  console.log(`Listening port ${PORT}`)
+})
---
+++ services/captionService.js
@@ -0,0 +1,73 @@
+// Imports
+const axios = require('axios')
+const { google } = require('googleapis')
+
+// Local variables
+const CAPTION_ID_URL = 'https://www.googleapis.com/youtube/v3/captions?part=id'
+const CAPTION_TEXT_URL = 'https://www.googleapis.com/youtube/v3/captions/'
+let token = null
+
+// Authenticate into google services
+const authenticate = async () => {
+  const auth = await google.auth.getClient({
+    scopes: [
+      'https://www.googleapis.com/auth/youtube.force-ssl',
+      'https://www.googleapis.com/auth/youtubepartner',
+    ]
+  })
+  accessToken = await auth.getAccessToken()
+  token = accessToken.token
+}
+
+// Fetch captions using proper APIs
+const getCaptions = async (videoId) => {
+  await authenticate()
+  // Contains list of different possible captions - one timestamp can contain multiple
+  // captions
+  const captionsResponse = await axios.get(`${CAPTION_ID_URL}&videoId=${videoId}&key=${process.env.API_KEY}`)
+
+  // For each possible caption, fetch that caption
+  const captionIds = captionsResponse.data.items.map((caption) => caption.id)
+  let captions = ''
+  for (let i = 0; i < captionIds.length; i++) {
+    const id = captionIds[i]
+    // Returns ostream, therefore we need the responseType to be arraybuffer
+    const options = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      responseType: 'arraybuffer'
+    }
+
+    // Fetch caption. Contains multiple timestamps and captions in a single string
+    const caption = await axios.get(`${CAPTION_TEXT_URL}${id}?&tlang=en&key=${process.env.API_KEY}`, options)
+    const captionAsString = String.fromCharCode.apply(null, new Uint16Array(caption.data))
+    captions += captionAsString
+    console.log('id', id)
+  }
+  const clusteredCaptions = {}
+  
+  // Cluster captions so that there won't be duplicates of same timestamp
+  captions.split('\n\n').forEach((caption) => {
+    const splittedCaption = caption.split('\n')
+    if (!splittedCaption[1]) {
+      splittedCaption[1] = ''
+    }
+    const timestamp = splittedCaption[0].split(',')[0]
+    if (!clusteredCaptions[timestamp]) {
+      clusteredCaptions[timestamp] = []
+    }
+
+    clusteredCaptions[timestamp].push(splittedCaption[1].trim())
+  })
+  const result = []
+
+  // Format result into a nice JSON format
+  Object.keys(clusteredCaptions).forEach((timestamp) => {
+    result.push({ timestamp, captions: clusteredCaptions[timestamp] })
+  })
+  
+  return result
+}
+
+module.exports = { getCaptions }
